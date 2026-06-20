56f59124439f39300a9420f57a53aa6e9a5ab1eb
andreea_zamfir55 <andreea_zamfir55@yahoo.com>
2026-06-20 17:45:33 +0300

    change variable value

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // this is my first JavaScript code!
 
-let javascriptIsFun = "true";
-console.log(javascriptIsFun);
+let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
-console.log(typeof true);
+//console.log(typeof true);
 console.log(typeof javascriptIsFun); 
-console.log(typeof 23);
-console.log(typeof "Jonas"); 
+//console.log(typeof 23);
+//console.log(typeof "Jonas"); 
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
